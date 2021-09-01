723543f140b90ce792b85ea13f6b911c08975eb0
Brandon Lewis <bclewis@outlook.com>
2021-09-01 12:02:41 -0400

    add morgan logger and helmet security

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import * as express from "express";
 import * as cors from "cors";
-// import * as helmet from "helmet";
+import * as helmet from "helmet";
+import * as morgan from "morgan";
 import todoRoutes from "./routes/todoRoute";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-// app.use(helmet());
+app.use(helmet());
+app.use(morgan("dev"));
 app.use(todoRoutes);
 
 export default app;
--- src/services/logger.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from "express";
-
-const logRequest = (req: Request): void => {
-  console.log(`${req.method} ${req.url}`);
-};
-
-export { logRequest };
