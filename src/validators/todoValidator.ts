a02750c9d33d9d8bdd7788b7d38a3fbbf5a71869
Brandon Lewis <bclewis@outlook.com>
2021-09-17 17:00:48 -0400

    decouple validations from todoService

--- src/validators/todoValidator.ts
+++ src/validators/todoValidator.ts
@@ -4,4 +4,22 @@ const isValidId = (id: string): boolean => {
   return !mongoose.Types.ObjectId.isValid(id);
 };
 
-export { isValidId };
+const isValidTextBody = (textBody: string): boolean => {
+  return textBody !== "" && textBody.length <= 255;
+};
+
+const validateId = (id: string) => {
+  if (isValidId(id)) {
+    throw new Error("Todo.id must be a valid mongoose ObjectId");
+  }
+};
+
+const validateTextBody = (textBody: string) => {
+  if (isValidTextBody(textBody)) {
+    throw new Error(
+      "Todo.textBody must be a non-empty string of at most 255 characters"
+    );
+  }
+};
+
+export { isValidId, isValidTextBody, validateId, validateTextBody };
